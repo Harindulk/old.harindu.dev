2e931c88b1555e8ad33cc364097e17f9582fda85
harindulk <harindulakshitha525@gmail.com>
2022-06-05 14:28:30 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+$('.button').click(function() {
+  $.ajax({
+      url: "",
+      context: document.body,
+      success: function(s,x){
+
+          $('html[manifest=saveappoffline.appcache]').attr('content', '');
+              $(this).html(s);
+      }
+  }); 
+});
+
 (function($) {
   "use strict";
 
