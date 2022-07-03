9b1cf0efcd409aed065dce40f7fe5266f39ea2af
harindulk <harindulakshitha525@gmail.com>
2022-07-04 03:40:00 +0530

    cleaned code

---
+++ assets/js/about.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('harindulk', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support Me',
+    'floating-chat.donateButton.background-color': '#323842',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
